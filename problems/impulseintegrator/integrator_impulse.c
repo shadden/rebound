2dbe2fdb5345a3cd68b692f02155be54efbbaf57
Hanno Rein <hanno@hanno-rein.de>
2012-11-19 21:35:21 -0500

    implemented not working yet

--- problems/impulseintegrator/integrator_impulse.c
+++ problems/impulseintegrator/integrator_impulse.c
@@ -69,73 +69,90 @@ void integrator_part1(){
 		vp[i].y = v[i].y;
 		vp[i].z = v[i].z;
 	}
-	{
+	for (int iterations=0;iterations<5;iterations++){
 		for (int i=0;i<N;i++){
 			vpp[i].x = v[i].x;
 			vpp[i].y = v[i].y;
 			vpp[i].z = v[i].z;
-			for (int j=0;j<N;j++){
-				if (i!=j){
-					{
-						struct xyz _xp;
-						_xp.x = xp[i].x - xp[j].x;
-						_xp.y = xp[i].y - xp[j].y;
-						_xp.z = xp[i].z - xp[j].z;
-						struct xyz _v;
-						_v.x = v[i].x - v[j].x;
-						_v.y = v[i].y - v[j].y;
-						_v.z = v[i].z - v[j].z;
-						double v2 = _v.x*_v.x + _v.y*_v.y + _v.z*_v.z; 
-						double y2 = _xp.x*_xp.x + _xp.y*_xp.y + _xp.z*_xp.z + softening*softening;
-						struct xyz _xp_v_tau;
-						_xp_v_tau.x = _xp.x-_v.x*dt;
-						_xp_v_tau.y = _xp.y-_v.y*dt;
-						_xp_v_tau.z = _xp.z-_v.z*dt;
-						double s = sqrt(_xp_v_tau.x*_xp_v_tau.x + _xp_v_tau.y*_xp_v_tau.y + _xp_v_tau.z*_xp_v_tau.z + softening*softening);
-						double _xp_v = _xp.x*_v.x+_xp.y*_v.y+_xp.z*_v.z;
-						double prefactor = G*particles[j].m/(s*(v2*y2-(_xp_v)));
-						struct xyz bracket;
-						bracket.x =  _v.x*(y2-_xp_v*dt) + _xp.x*(v2*dt-_xp_v);
-						bracket.y =  _v.y*(y2-_xp_v*dt) + _xp.y*(v2*dt-_xp_v);
-						bracket.z =  _v.z*(y2-_xp_v*dt) + _xp.z*(v2*dt-_xp_v);
-						vpp[i].x += -prefactor*bracket.x;
-						vpp[i].y += -prefactor*bracket.y;
-						vpp[i].z += -prefactor*bracket.z;
-					}
-					{
-						struct xyz _xp;
-						_xp.x = xp[i].x - xp[j].x;
-						_xp.y = xp[i].y - xp[j].y;
-						_xp.z = xp[i].z - xp[j].z;
-						struct xyz _v;
-						_v.x = v[i].x - v[j].x;
-						_v.y = v[i].y - v[j].y;
-						_v.z = v[i].z - v[j].z;
-						double v2 = _v.x*_v.x + _v.y*_v.y + _v.z*_v.z; 
-						double y2 = _xp.x*_xp.x + _xp.y*_xp.y + _xp.z*_xp.z + softening*softening;
-						struct xyz _xp_v_tau;
-						_xp_v_tau.x = _xp.x;
-						_xp_v_tau.y = _xp.y;
-						_xp_v_tau.z = _xp.z;
-						double s = sqrt(_xp_v_tau.x*_xp_v_tau.x + _xp_v_tau.y*_xp_v_tau.y + _xp_v_tau.z*_xp_v_tau.z + softening*softening);
-						double _xp_v = _xp.x*_v.x+_xp.y*_v.y+_xp.z*_v.z;
-						double prefactor = G*particles[j].m/(s*(v2*y2-(_xp_v)));
-						struct xyz bracket;
-						bracket.x =  _v.x*(y2) + _xp.x*(-_xp_v);
-						bracket.y =  _v.y*(y2) + _xp.y*(-_xp_v);
-						bracket.z =  _v.z*(y2) + _xp.z*(-_xp_v);
-						vpp[i].x -= -prefactor*bracket.x;
-						vpp[i].y -= -prefactor*bracket.y;
-						vpp[i].z -= -prefactor*bracket.z;
-					}
-				}
-			}
 			xpp[i].x = x[i].x+v[i].x*dt;
 			xpp[i].y = x[i].y+v[i].y*dt;
 			xpp[i].z = x[i].z+v[i].z*dt;
 			for (int j=0;j<N;j++){
 				if (i!=j){
-					{
+					struct xyz _xp;
+					_xp.x = xp[i].x - xp[j].x;
+					_xp.y = xp[i].y - xp[j].y;
+					_xp.z = xp[i].z - xp[j].z;
+					struct xyz _v;
+					_v.x = v[i].x - v[j].x;
+					_v.y = v[i].y - v[j].y;
+					_v.z = v[i].z - v[j].z;
+					double _v2 = _v.x*_v.x + _v.y*_v.y + _v.z*_v.z; 
+					double _v1  = sqrt(_v2); 
+					double _v3 = _v1*_v1*_v1;
+					double y2 = _xp.x*_xp.x + _xp.y*_xp.y + _xp.z*_xp.z + softening*softening;
+					double _xp_v = _xp.x*_v.x+_xp.y*_v.y+_xp.z*_v.z;
+					{	// tau = dt
+						struct xyz _xp_v_tau;
+						_xp_v_tau.x = _xp.x-_v.x*dt;
+						_xp_v_tau.y = _xp.y-_v.y*dt;
+						_xp_v_tau.z = _xp.z-_v.z*dt;
+						double s = sqrt(_xp_v_tau.x*_xp_v_tau.x + _xp_v_tau.y*_xp_v_tau.y + _xp_v_tau.z*_xp_v_tau.z + softening*softening);
+						double prefactor = G*particles[j].m/(s*(_v2*y2-(_xp_v)));
+						
+						// Calculating the new xv prime
+						vpp[i].x += -prefactor*(_v.x*(y2-_xp_v*dt) + _xp.x*(_v2*dt-_xp_v));
+						vpp[i].y += -prefactor*(_v.y*(y2-_xp_v*dt) + _xp.y*(_v2*dt-_xp_v));
+						vpp[i].z += -prefactor*(_v.z*(y2-_xp_v*dt) + _xp.z*(_v2*dt-_xp_v));
+
+
+						// Calculating the new x prime
+						// First term in bracket
+						xpp[i].x += prefactor * _xp.x * (y2-_xp_v*dt);
+						xpp[i].y += prefactor * _xp.y * (y2-_xp_v*dt);
+						xpp[i].z += prefactor * _xp.z * (y2-_xp_v*dt);
+						
+						// Second term in bracket
+						xpp[i].x -= prefactor * _v.x / _v2 * (y2*_xp_v + _v2*y2*dt-2.*dt*_xp_v*_xp_v);
+						xpp[i].y -= prefactor * _v.y / _v2 * (y2*_xp_v + _v2*y2*dt-2.*dt*_xp_v*_xp_v);
+						xpp[i].z -= prefactor * _v.z / _v2 * (y2*_xp_v + _v2*y2*dt-2.*dt*_xp_v*_xp_v);
+						
+						// Third term in bracket
+						double _log = log(_v2*dt-_xp_v+_v1*s);
+						xpp[i].x -= G*particles[j].m*_v.x/_v3 * _log;
+						xpp[i].y -= G*particles[j].m*_v.y/_v3 * _log;
+						xpp[i].z -= G*particles[j].m*_v.z/_v3 * _log;
+					}
+					{	// tau = 0
+						struct xyz _xp_v_tau;
+						_xp_v_tau.x = _xp.x;
+						_xp_v_tau.y = _xp.y;
+						_xp_v_tau.z = _xp.z;
+						double s = sqrt(_xp_v_tau.x*_xp_v_tau.x + _xp_v_tau.y*_xp_v_tau.y + _xp_v_tau.z*_xp_v_tau.z + softening*softening);
+						double prefactor = G*particles[j].m/(s*(_v2*y2-(_xp_v)));
+
+						// Calculating the new xv prime
+						vpp[i].x -= -prefactor*(_v.x*(y2) + _xp.x*(-_xp_v));
+						vpp[i].y -= -prefactor*(_v.y*(y2) + _xp.y*(-_xp_v));
+						vpp[i].z -= -prefactor*(_v.z*(y2) + _xp.z*(-_xp_v));
+						
+
+						// Calculating the new x prime
+						// First term in bracket
+						xpp[i].x -= prefactor * _xp.x * (y2);
+						xpp[i].y -= prefactor * _xp.y * (y2);
+						xpp[i].z -= prefactor * _xp.z * (y2);
+						
+						// Second term in bracket
+						xpp[i].x += prefactor * _v.x / _v2 * (y2*_xp_v);
+						xpp[i].y += prefactor * _v.y / _v2 * (y2*_xp_v);
+						xpp[i].z += prefactor * _v.z / _v2 * (y2*_xp_v);
+						
+						// Third term in bracket
+						double _log = log(-_xp_v+_v1*s);
+						xpp[i].x += G*particles[j].m*_v.x/_v3 * _log;
+						xpp[i].y += G*particles[j].m*_v.y/_v3 * _log;
+						xpp[i].z += G*particles[j].m*_v.z/_v3 * _log;
 					}
 				}
 			}
