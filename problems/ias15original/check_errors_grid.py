6fa8eb25063123e153413ac542c6a5c80d2a8427
Dave <dddave@gmail.com>
2014-06-23 15:45:55 -0400

    updated linear plot for checking errors with IAS15 and Everhart

--- problems/ias15original/check_errors_grid.py
+++ problems/ias15original/check_errors_grid.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 ERRORS_FNAME = "errors_grid.txt"
 N_MASS = 11
 N_SIZE = 11
-N_ORBITS = 10.
+NN_SIZE = 21
+N_ORBITS = 1000
 MASS_MIN = 1.e-3
 MASS_MAX = 1.e3
 SIZE_MIN = 1.e-3
@@ -17,8 +18,6 @@ REL_EN_ERR = dict(ndx= 7,text="Rel. Energy Error",text2="en_err")
 REL_KE_ERR = dict(ndx= 8,text="Rel. KE Error",text2="ke_err")
 REL_PE_ERR = dict(ndx= 9,text="Rel. PE Error",text2="pe_err")
 REL_VV_ERR = dict(ndx=11,text="Rel. Vel Error",text2="v_err")
-IAS15_eps = 100.
-EVERHART_eps = 1.e-4
 
 def call_rebound_grid(mass_scale_vec,size_scale_vec,
                       ias15new=True,rel_errors_list=[REL_EN_ERR]):
@@ -73,6 +72,55 @@ def call_rebound_grid(mass_scale_vec,size_scale_vec,
                                                           this_error))
     return dict(errors_A=errors_A,rel_errors_list=rel_errors_list)
 
+def call_rebound_curve(size_scale_vec,ias15new=True,
+                       rel_errors_list=[REL_EN_ERR],
+                       make=True):
+    errors_v = {}
+    errors_filename = {}
+    version = "ias15"
+    if ias15new:
+        rebound_error_fname = "energy_ias15variable.txt"
+    else:
+        rebound_error_fname = "energy_ias15original.txt"
+        version += "original"
+    for rel_error in rel_errors_list:
+        key = rel_error["text2"]
+        this_v = np.zeros(len(size_scale_vec))
+        errors_v[key] = np.NaN * this_v
+        this_err_fname = ERRORS_FNAME[:-4] + '_' + version \
+                         + '_' + key + '_v' + ERRORS_FNAME[-4:]
+        errors_filename[key] = this_err_fname
+        try: # remove this error file if it exists
+            os.remove(this_err_fname)
+        except OSError: # swallow error and do nothing
+            pass
+    # compile (make) binary
+    if make:
+        make_command = "make %s"%version
+        subprocess.call(make_command,shell=True)
+    for size_scale_ndx in xrange(len(size_scale_vec)):
+        size_scale = size_scale_vec[size_scale_ndx]
+        try: # remove the REBOUND error file if it exists
+            os.remove(rebound_error_fname)
+        except OSError: # swallow error and do nothing
+            pass
+        runstring = "./nbody"
+        runstring += " --scale_a=%8.4e"%size_scale
+        runstring += " --tmax_factor=%4.2f"%N_ORBITS
+        if ias15new: runstring += " --epsilon=%4.2e"%IAS15_eps
+        else:        runstring += " --epsilon=%4.2e"%EVERHART_eps
+        print runstring
+        subprocess.call(runstring,shell=True)
+        # harvest errors from the rebound run
+        rebound_errors = np.loadtxt(rebound_error_fname)
+        for rel_error in rel_errors_list:
+            key = rel_error["text2"]
+            this_error = rebound_errors[rel_error["ndx"]]
+            errors_v[key][size_scale_ndx] = this_error
+            with open(errors_filename[key],'a') as errors_f:
+                errors_f.write("%8.4e %8.4e\n"%(size_scale,this_error))
+    return dict(errors_v=errors_v,rel_errors_list=rel_errors_list)
+
 def generate_grid(ias15new=True,rel_errors_list=[REL_EN_ERR]):
     mass_scale_vec = np.logspace(np.log10(MASS_MIN),np.log10(MASS_MAX),N_MASS)
     size_scale_vec = np.logspace(np.log10(SIZE_MIN),np.log10(SIZE_MAX),N_SIZE)
@@ -82,6 +130,13 @@ def generate_grid(ias15new=True,rel_errors_list=[REL_EN_ERR]):
     return dict(masses_A = masses_A,
                 sizes_A = sizes_A,
                 grid_dict = grid_dict)
+
+def generate_curve(ias15new=True,rel_errors_list=[REL_EN_ERR],make=True):
+    size_scale_vec = np.logspace(np.log10(SIZE_MIN),np.log10(SIZE_MAX),NN_SIZE)
+    curve_dict = call_rebound_curve(size_scale_vec,ias15new=ias15new,
+                                    rel_errors_list=rel_errors_list,make=make)
+    return dict(size_scale_vec = size_scale_vec,
+                curve_dict = curve_dict)
 
 def display_grid(info_dict,ias15new=True):
     masses_A = info_dict["masses_A"]
@@ -134,18 +189,108 @@ def display_grid(info_dict,ias15new=True):
             plt.savefig(plot_fname)
             plt.close()
 
+
 if __name__=="__main__":
     ias15new_list = [False,True]
     rel_errors_list = [REL_EN_ERR,REL_KE_ERR,REL_PE_ERR]
-    for ias15new_bool in ias15new_list:
-        ret_dict = generate_grid(ias15new=ias15new_bool,
-                                 rel_errors_list=rel_errors_list)
-        display_grid(ret_dict,ias15new=ias15new_bool)
-
+    plot_type = "lineplot"
+    if plot_type == "gridplot":
+        for ias15new_bool in ias15new_list:
+            ret_dict = generate_grid(ias15new=ias15new_bool,
+                                     rel_errors_list=rel_errors_list)
+            display_grid(ret_dict,ias15new=ias15new_bool)
+    ####################### 1-D plot
+    elif plot_type == "lineplot":
+        plt.ion()
+        IAS15_eps_vec = [10., 1, 0.1, 0.01]
+        make = True
+        for IAS15_eps in IAS15_eps_vec:
+            ret_dict_new = generate_curve(ias15new=True,
+                                          rel_errors_list=rel_errors_list,
+                                          make=make)
+            if make: make = False
+            size_scale_vec = ret_dict_new["size_scale_vec"]
+            en_errors_new = np.abs(ret_dict_new["curve_dict"]["errors_v"]["en_err"])
+            plt.loglog(size_scale_vec,en_errors_new,'b-',linewidth=1)
+            ytn = np.mean(en_errors_new)/3;
+            text_str = "$\epsilon$ = " + str(IAS15_eps)
+            plt.text(1.e2,ytn,text_str,
+                     size=12,color='b',
+                     fontweight="bold",
+                     backgroundcolor='w')
+        EVERHART_eps_vec = [1.e-4, 1.e-5, 1.e-6, 1.e-11]
+        make = True
+        for EVERHART_eps in EVERHART_eps_vec:
+            ret_dict_orig = generate_curve(ias15new=False,
+                                           rel_errors_list=rel_errors_list,
+                                           make=make)
+            if make: make = False
+            en_errors_orig = np.abs(ret_dict_orig["curve_dict"]["errors_v"]["en_err"])
+            plt.loglog(size_scale_vec,en_errors_orig,'r-',linewidth=1)
+            yto = np.mean(en_errors_orig[:NN_SIZE/3])/3
+            text_str = "$\epsilon$ = " + "%1.e"%EVERHART_eps
+            plt.text(6.e-3,yto,text_str,
+                     size=12,color='r',
+                     fontweight="bold",
+                     backgroundcolor='w')
+        plt.xlabel("Size Scale",size=12)
+        plt.ylabel("Relative Energy Error",size=12)
+        plt.text(3.e-3,2e-3,"Everhart time stepping",
+                 size=12,color='r',
+                 fontweight="bold",
+                 backgroundcolor='w',
+                 horizontalalignment="left")
+        plt.text((1./3)*1.e3,2e-3,"IAS15 time stepping",
+                 size=12,color='b',
+                 fontweight="bold",
+                 backgroundcolor='w',
+                 horizontalalignment="right")
+        thisaxis = plt.axis()
+        plt.axis([thisaxis[0],thisaxis[1],10**-13,10**-1])
+        text_str = "Errors over " + str(N_ORBITS) + " orbits"
+        plt.text(1.,1e-2,text_str,
+                 size=14,
+                 horizontalalignment="center",
+                 fontweight="bold")
+        plt.show()
+        plt.savefig("errors_sizescale_1d.pdf")
 
 
 
 # old
+
+# def display_curve(info_dicts,ias15new_list):
+#     size_scale_vec  = info_dicts[0]["size_scale_vec"]
+#     # loop over the different errors
+#     #plt.ion()
+#     ndx = -1
+#     for thisdict in info_dicts:
+#         ndx += 1
+#         ias15new = ias15new_list[ndx]
+#         curve_dict = thisdict["curve_dict"]
+#         errors_v = curve_dict["errors_v"]
+#         rel_errors_list = curve_dict["rel_errors_list"]
+#         for rel_error in rel_errors_list:
+#             key = rel_error["text2"]
+#             this_err = errors_v[key]
+#             abs_err = np.abs(this_err) + 1.00001e-17
+#             if ias15new:
+#                 plt.loglog(size_scale_vec,abs_err,'-')
+#             else:
+#                 print "###################"
+#                 print abs_err
+#                 print "###################"
+#                 print size_scale_vec
+#                 plt.plot(size_scale_vec,abs_err,'--')
+#                 plt.show()
+#             title_str = "Errors vs. Size Scale"
+#             plt.title(title_str,size=16)
+#             plt.xlabel("Size Scale",size=12)
+#             ylabel_str = "log$_{10}$[|" + rel_error["text"] + "|] "
+#             plt.ylabel(ylabel_str,size=12)
+#             plt.show()
+#             plot_fname = "errors_v_" + key + ".pdf"
+#             plt.savefig(plot_fname)
 
 # def read_grid(ias15new=True):
 #     version = "ias15"
