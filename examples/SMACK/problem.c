bde912fa2f4a88ad659e89cc60deb93157ca7067
Erika Nesvold <ernesvold@gmail.com>
2014-07-02 11:32:33 -0400

    Added lots of comments to problem.c

--- examples/SMACK/problem.c
+++ examples/SMACK/problem.c
@@ -47,9 +47,10 @@ extern int Nmax;
 
 #define numbins 21
 #define extra 30
-const double logbinsize = 0.1;
-const int shift = 3;
 
+const int nowave = 1; // Flag to remove size distribution wave by extrapolation
+const double logbinsize = 0.1; // bin size for size distribution
+const int shift = 3;		// for building size distribution
 const int rho = 3.0E3;		// Density (kg m^-3)
 
 double sizebins[numbins];		// size bins
@@ -67,15 +68,13 @@ double powsizebins[numbins];
 
 void problem_init(int argc, char* argv[]){
 	// Setup constants
-	N_active = 1;
-#ifndef COLLISIONS_NONE	
-	N_tree_fixed = 1;
-#endif
-	boxsize = 180;	// AU
+	N_active = 1;		// Number of active particles
+	N_tree_fixed = 1;	// Number of fixed particles in tree
+	boxsize = 180;		// AU
 	root_nx = 2;
 	root_ny = 2;
-	dt = 1.0/0.159; // Timestep (years->code units)
-	tmax = (1.0e2+1)*dt;
+	dt = 1.0/0.159;		// Timestep (years->code units)
+	tmax = (1.0e2+1)*dt;// Max time
 	init_box();
 	
 	// Change collision_resolve routing from default.
@@ -87,59 +86,55 @@ void problem_init(int argc, char* argv[]){
 	star.vx 	= 0; star.vy 	= 0; star.vz 	= 0;
 	star.ax 	= 0; star.ay 	= 0; star.az 	= 0;
 	star.m 		= 1.0;
-#ifndef COLLISIONS_NONE
 	star.r		= 1.0*0.0046491;	// Rsun
-#endif
+
 	// Add the star to the particle array, but not to the tree
 	Nmax += 128;
 	particles = realloc(particles,sizeof(struct particle)*Nmax);
 	particles[N] = star;
 	N++;
 	
-	// Collision parameters
-	double p0 = 2.5;
-	int nowave = 1;
-	
-	// Orbital parameters
-	double amin = 50.0;
+	// Orbital parameters of disk
+	double amin = 50.0;			// Semi-major axis
 	double amax = 60.0;
-	double emin = 0.0;
-	double emax = 0.1;
-	double imin = 0.0;
+	double emin = 0.0;			// Eccentricity
+	double emax = 0.1;	
+	double imin = 0.0;			// Inclination
 	double imax = emax/2.; 
-	double Orange = 2*M_PI;	
-	double orange = 2*M_PI;
-	double Mrange = 2*M_PI;
+	double Orange = 2*M_PI;		// Longitude of ascending node
+	double orange = 2*M_PI;		// Argument of periapse
+	double Mrange = 2*M_PI;		// Mean anomaly
 	double a,e,i,OMEGA,omega,M;
 	
-	// Swarm parameters
-	int numswarms = 1000;
-	double swarmr = 1.0;
-	double height = 2.77137; // ecc 0.2
-	double factor = 3*height/(4*swarmr);
-	double initOD = 1.0e-2; // Initial optical depth
-	double Aeach = initOD*M_PI*swarmr*swarmr*1.5e11*1.5e11/factor;  
+	// Superparticle parameters
+	int numswarms = 1000;		// Number of superparticles
+	double swarmr = 1.0;		// Superparticle radius
+	double height = 2.77137;	// Height of disk
+	double factor = 3*height/(4*swarmr);	// Filling factor
+	double initOD = 1.0e-2;					// Initial optical depth
+	double Aeach = initOD*M_PI*swarmr*swarmr*1.5e11*1.5e11/factor; // Cross-sectional area 
+	double p0 = 2.5; // Initial size distribution index
 	double distsum;
 	double C;
 	double sdist[numbins];
-	double smass;
+	double smass;	
 	
-	// Size bin array
+	// Create size bin arrays
 	for (int i=0;i<numbins;i++){
 		sizebins[i] = pow(10,i*logbinsize-shift);
 	}
-	if (nowave) {
+	if (nowave) { // Extrapolate to smaller sizes to remove size distribution wave
 		for (int i=0; i<numbins+extra; i++) {
 			bigbins[i] = pow(10,i*logbinsize-shift-extra*logbinsize);
 		}
 	}
 	
-	// Collision energy calculations
-	const double Scon = 3.5e3;                    // strength regime coefficient
-	const double Gcon = 3.0e-8;                   // gravity regime coefficient
-	const double spow = -0.38;                    // strength regime exponent
-	const double gpow = 1.36;                     // gravity regime exponent
-	const double fKE = 1.0;						// Fraction of kinetic energy used
+	// Collision energy pre-calculations
+	const double Scon = 3.5e3;	// Strength regime coefficient
+	const double Gcon = 3.0e-8;	// Gravity regime coefficient
+	const double spow = -0.38;	// Strength regime exponent
+	const double gpow = 1.36;	// Gravity regime exponent
+	const double fKE = 1.0;		// Fraction of kinetic energy used
 	{
 		double bigvolume[numbins+extra];	// extrapolated volume bins
 		for (int i=0; i<numbins+extra; i++) {
@@ -155,7 +150,7 @@ void problem_init(int argc, char* argv[]){
 		}
 	}
 	
-	// Fragment distribution caculations
+	// Fragment distribution pre-caculations
 	const double n0 = 2.8;			// Index of fragment distribution
 	for (int j = 0; j < numbins+extra; j++) {
 		powbigbins[j] = pow(bigbins[j],3.-n0);
@@ -170,6 +165,7 @@ void problem_init(int argc, char* argv[]){
 #else
 	for(int N=0;N<numswarms;N++) {
 #endif
+		// Create orbital element distributions
 		a = tools_uniform(amin,amax);
 		e = tools_uniform(emin,emax);
 		i = tools_uniform(imin,imax);
@@ -177,33 +173,31 @@ void problem_init(int argc, char* argv[]){
 		omega = tools_uniform(0,orange);
 		M = tools_uniform(0,Mrange);
 		
-		// Size distributions
-		smass = 0.0;
+		// Fill size distributions
+		smass = 0.0;	// Superparticle mass
 		distsum = 0.0;
 		for (int j=0; j<numbins; j++) {
 			distsum += pow(bigbins[j],-1*p0)*pow(bigbins[j],2);
 		}
-		C = (4*Aeach)/(M_PI*distsum);
+		C = (4*Aeach)/(M_PI*distsum);	// Normalize size distribution
 		for (int i=0; i<numbins; i++) {
 			sdist[i] = C*pow(sizebins[i],-p0);
 			smass += (M_PI/6)*rho*pow(sizebins[i],3)*sdist[i];
 		}
 		
-		smass /= 1.98892e30; // convert to solar units
+		smass /= 1.98892e30; // Convert superparticles mass to solar units
 		struct particle p;
-		p = tools_orbit2p(a,e,i,omega,OMEGA,M,star.m,smass);
-		p.m = smass;
-#ifndef COLLISIONS_NONE
-		p.r = swarmr;
-		p.number = N + N_active;
+		p = tools_orbit2p(a,e,i,omega,OMEGA,M,star.m,smass); // Calculate Cartesian coords
+		p.m = smass;	// Superparticle mass
+		p.r = swarmr;	// Superparticle radius
+		p.number = N + N_active;	// Superparticle number
 #ifdef MPI
 		p.number = N + N_active + mpi_id*numswarms/mpi_num;
 #endif
-		p.ncol = 0;
+		p.ncol = 0;	// Number of collisions
 		for (int i=0; i<numbins; i++) {
-			p.sdist[i] = sdist[i];
-		}
-#endif		
+			p.sdist[i] = sdist[i];	// Superparticle size distribution
+		}	
 		particles_add(p);
 		
 #ifdef MPI			
