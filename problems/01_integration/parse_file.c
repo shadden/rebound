feb70e258937cddbb2343ca0ad770e2641775c8c
Samuel Hadden <shadden1107@gmail.com>
2014-07-31 11:27:55 -0500

    01_integration implements read-in of parameters and planet data

--- problems/01_integration/parse_file.c
+++ problems/01_integration/parse_file.c
@@ -2,34 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void parse_cartesian_data(FILE *fi,int npl, double masses[npl], double pos[npl][3], double vel[npl][3]){
+void parse_param_data(FILE *fi,int* npl,double* dt,int* N_dt_out,int* N_dt_timing){
+	if(fscanf(fi,"%d",npl)!=1)
+		printf("Improper value, line 1");
+	if(fscanf(fi,"%lf",dt)!=1)
+		printf("Improper value, line 2");
+	if(fscanf(fi,"%d",N_dt_out)!=1)
+		printf("Improper value, line 3");
+	if(fscanf(fi,"%d",N_dt_timing)!=1)
+		printf("Improper value, line 4");
+	
+}
+void parse_cartesian_data(FILE *fi,int Npl, double masses[Npl], double ** pos, double **vel){
 	double mass;
 	double x,y,z;
 	double vx,vy,vz;
-	
 	int i =0;
-	while( fscanf(fi,"%lf %lf %lf %lf %lf %lf %lf",&mass,&x,&y,&z,&vx,&vy,&vz)==7){
+	for(i=0; i<Npl;i++){
+		if ( fscanf(fi,"%lf %lf %lf %lf %lf %lf %lf",&mass,&x,&y,&z,&vx,&vy,&vz)!=7){
+			printf("Error in planet file, line %d\n",i+1);
+			exit(1);
+		}
 		masses[i] = mass;
 		pos[i][0] = x;	pos[i][1] = y;	pos[i][2] = z;
 		vel[i][0] = vx;	vel[i][1] = vy;	vel[i][2] = vz;
-		i++;
 	}
 }
 
-int main()
-{
-	FILE * fi;
-	double pos[5][3],vel[5][3], masses[5];
-
-	fi = fopen("test.dat","r");
-	
-	parse_cartesian_data(fi,5,masses,pos,vel);
-	int i;
-	for (i=0;i<5;i++){
-		printf("%d , %lf\n",i,masses[i]);
-		printf("%lf %lf %lf\n",pos[i][0],pos[i][1],pos[i][2]);
-		printf("%lf %lf %lf\n",vel[i][0],vel[i][1],vel[i][2]);
-	}
-	return(0);
-}
--- problems/01_integration/problem.c
+++ problems/01_integration/problem.c
@@ -35,45 +35,93 @@
 #include "communication_mpi.h"
 #include "tree.h"
 #include "tools.h"
+#include <assert.h>
 
-int Npl,N_dt_out,N_dt_timing;
-char* parfile,posfile;
+int i,Npl,N_dt_out,N_dt;
+double dtdump;
+char *parfile,*posfile;
+
+// Parsing Functions
+void parse_param_data(FILE *fi,int* npl,double* dt,int* N_dt_out,int* N_dt_timing){
+	if(fscanf(fi,"%d",npl)!=1)
+		printf("Improper value, line 1");
+	if(fscanf(fi,"%lf",dt)!=1)
+		printf("Improper value, line 2");
+	if(fscanf(fi,"%d",N_dt_out)!=1)
+		printf("Improper value, line 3");
+	if(fscanf(fi,"%d",N_dt_timing)!=1)
+		printf("Improper value, line 4");
+	
+}
+void parse_cartesian_data(FILE *fi,int Npl, double masses[Npl], double ** pos, double **vel){
+	double mass;
+	double x,y,z;
+	double vx,vy,vz;
+	int i =0;
+	for(i=0; i<Npl;i++){
+		if ( fscanf(fi,"%lf %lf %lf %lf %lf %lf %lf",&mass,&x,&y,&z,&vx,&vy,&vz)!=7){
+			printf("Error in planet file, line %d\n",i+1);
+			exit(1);
+		}
+		masses[i] = mass;
+		pos[i][0] = x;	pos[i][1] = y;	pos[i][2] = z;
+		vel[i][0] = vx;	vel[i][1] = vy;	vel[i][2] = vz;
+	}
+}
+//
 
 void problem_init(int argc, char* argv[]){
-	if (argc>1){ 						// Try to read boxsize from command line
-		parfile = argv[1];
-		posfile = argv[2];
-	}else{
-		boxsize = 100;
-	}
+
+	assert(argc==3);	
+	parfile = argv[1];
+	posfile = argv[2];
+	boxsize=100;
 	init_box();
 
-	parse_param_data(fopen(parfile,"r"),Npl,N_dt_out,N_dt_timing);
-	
-	double mass[Npl],pos[Npl][3],vel[Npl][3];
-	parse_cartesian_data(fopen(posefile,"r"),Npl,mass,pos,vel);
+	parse_param_data(fopen(parfile,"r"),&Npl,&dt,&N_dt,&N_dt_out);
+	// Setup constants
+	tmax = N_dt * dt;
+	dtdump = N_dt_out *dt;
+
+	double **pos,**vel,*masses;
+	pos = (double **)malloc(sizeof(double *)*Npl);
+	vel = (double **)malloc(sizeof(double *)*Npl);
+	for(i=0; i < Npl; i++) {
+	  pos[i] = (double *)malloc(sizeof(double)*3);
+	  vel[i] = (double *)malloc(sizeof(double)*3);
+	}
+	masses = (double *)malloc(sizeof(double)*Npl);
+
+	parse_cartesian_data(fopen(posfile,"r"),Npl,masses,pos,vel);
 
 	for (int i=0; i < Npl; i++){
 		struct particle p;
 		p.x  = pos[i][0]; 		p.y  = pos[i][1];	 	p.z  = pos[i][2];
 		p.vx = vel[i][0]; 		p.vy = vel[i][1];	 	p.vz = vel[i][2];
 		p.ax = 0; 			p.ay = 0; 			p.az = 0;
-		p.m  = mass[i];
+		p.m  = masses[i];
 		particles_add(p); 
 	}
 	
 	system("rm -f orbits.txt");
+	for(i=0; i < Npl; i++) {
+	  free(pos[i]);
+	  free(vel[i]);
+	}
+	free(pos);
+	free(vel);
+	free(masses);
 	
 }
 
 void problem_inloop(){
+
 }
 
 void problem_output(){
-	if (output_check(N_dt_timing*dt){
+
+	if (output_check(dtdump)){
 		output_timing();
-	}
-	if (output_check(N_dt_out*dt){
 		output_append_orbits("orbits.txt");
 	}
 }
